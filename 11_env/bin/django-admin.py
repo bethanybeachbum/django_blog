52f5bf0319d965faa0b6634442a0d3f595119692
“Mark <allenmarkbrown@gmail.com>
2020-03-29 20:04:46 -0400

    first commit - Video #6 completed - Corey Shafer

---
+++ 11_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/allenmarkbrown/PROJECTS/django_project/11_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+# urls.py inside of blogs director
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+]
