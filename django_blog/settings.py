a1d888dd8f0f14f456c5ce0dedbf3493e84785e8
“Mark <allenmarkbrown@gmail.com>
2020-04-10 14:04:55 -0400

    updated static root

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -124,9 +124,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # We put this in to change where pics are saved
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
